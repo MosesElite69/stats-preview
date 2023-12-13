258f8fc7d93b14db77ecab3047fa91be1a723539
Elliot <elliotedwinmoses2020@gmail.com>
2023-12-13 18:10:01 +0200

    started

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="bold flex justify-center text-orange-600 underline bg-blue-400 " >Hello Broooo</h1>
+    </>
+  )
+}
+
+export default App
